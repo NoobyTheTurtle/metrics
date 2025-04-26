b63c25d985122b8c57b433e641572317cb30a9fb
Alexey Mikhaylov <black98alex@gmail.com>
2025-04-26 12:41:47 +0300

    Refactor PingHandler to remove context timeout

--- internal/handler/ping/ping_handler.go
+++ internal/handler/ping/ping_handler.go
@@ -1,17 +1,12 @@
 package ping
 
 import (
-	"context"
 	"net/http"
-	"time"
 )
 
 func (h *Handler) PingHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx, cancel := context.WithTimeout(r.Context(), 1*time.Second)
-		defer cancel()
-
-		err := h.db.Ping(ctx)
+		err := h.db.Ping(r.Context())
 		if err != nil {
 			h.logger.Error("Database connection failed: %v", err)
 			http.Error(w, "Database connection failed", http.StatusInternalServerError)
